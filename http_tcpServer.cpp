6e3cd3e33890d6a19068821b701b09a88cf564fb
Marius Andrian <marius.atj@gmail.com>
2024-03-24 22:42:43 +0800

    feat: add socket and bind

--- http_tcpServer.cpp
+++ http_tcpServer.cpp
@@ -15,12 +15,25 @@ void exitWithError(const std::string &message) {
 } // namespace
 
 namespace http {
-TcpServer::TcpServer() {}
+TcpServer::TcpServer(std::string ip_address, int port)
+    : m_ip_address(ip_address), m_port(port), m_socket(), m_new_socket(),
+      m_incomingMessage(), m_socketAddress(),
+      m_socketAddress_len(sizeof(m_socketAddress)), m_serverMessage() {
+  m_socketAddress.sin_family = PF_INET;
+  m_socketAddress.sin_port = htons(m_port);
+  m_socketAddress.sin_addr.s_addr = inet_addr(m_ip_address.c_str());
+}
 
 int TcpServer::StartServer() {
-  m_socket = socket(AF_INET, SOCK_STREAM, 0);
+  m_socket = socket(PF_INET, SOCK_STREAM, 0);
   if (m_socket < 0) {
     exitWithError("Cannot create socket");
+    return 1;
+  }
+
+  // Must cast sockaddr_in* to sockaddr*.
+  if (bind(m_socket, (sockaddr *)&m_socketAddress, m_socketAddress_len) < 0) {
+    exitWithError("Cannot bind socket");
     return 1;
   }
   return 0;
@@ -31,5 +44,9 @@ void TcpServer::StopServer() {
   exit(0);
 }
 
-TcpServer::~TcpServer() {}
+TcpServer::~TcpServer() {
+  close(m_socket);
+  close(m_new_socket);
+  exit(0);
+}
 } // namespace http
--- http_tcpServer.h
+++ http_tcpServer.h
@@ -1,25 +1,24 @@
 #pragma once
 
 #include <string>
+#include <netinet/in.h>
 
-namespace http
-{
-    class TcpServer
-    {
-    public:
-        TcpServer(std::string ip_address, int port);
-        int StartServer();
-        void StopServer();
-        ~TcpServer();
+namespace http {
+class TcpServer {
+public:
+  TcpServer(std::string ip_address, int port);
+  int StartServer();
+  void StopServer();
+  ~TcpServer();
 
-    private:
-        std::string m_ip_address;
-        int m_port;
-        int m_socket;
-      int m_new_socket;
-      long m_incomingMessage;
-    // struct sockaddr_in m_socketAddress;
-    unsigned int m_socketAddres_len;
-    std::string m_serverMessage;
-    };
-}
+private:
+  std::string m_ip_address;
+  int m_port;
+  int m_socket;
+  int m_new_socket;
+  long m_incomingMessage;
+  struct sockaddr_in m_socketAddress;
+  unsigned int m_socketAddress_len;
+  std::string m_serverMessage;
+};
+} // namespace http
